62ded00edc19a0340b005e4ca2e909934ece1ff0
looten <olsson.joel@live.com>
2021-12-29 20:50:53 +0100

    2021: Day 4 part 1 working

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -1,6 +1,6 @@
 
 filename = "/home/looten/workspace/advent_of_code/2021/day4/test_data.txt"
-#filename = "/home/looten/workspace/advent_of_code/2021/day4/input.txt"
+filename = "/home/looten/workspace/advent_of_code/2021/day4/input.txt"
 
 def readfile():
     draw_order = []
@@ -15,6 +15,7 @@ def readfile():
         game = []
         idx = 0
 
+    
         for line in lines[1:]:
             line = line.replace("\n", "")
             if not line:
@@ -33,19 +34,18 @@ def readfile():
                 game = []
                 idx += 1
                         
-
     return draw_order, guess_list
 
 def draw_nr(draw_order, guess_list):
     print("Starting to draw")
     for draw in draw_order:
         call_draw(draw, guess_list)
-        print_guess(guess_list)
-        check_for_winner(guess_list)
+        #print_guess(guess_list)
+        check_for_winner(draw, guess_list)
 
 def print_guess(guess_list):
     for i in range(len(guess_list)):
-        print(f"game {i}")
+        print(f"Game list {i}")
         for d in guess_list[i]:
             print(f" {d}")
 
@@ -55,103 +55,45 @@ def call_draw(draw, guess_list):
             if draw in data:
                 data[draw] = 'x'
 
-def check_for_winner(guess_list):
+def check_for_winner(draw, guess_list):
     for game in guess_list:
         winner = check_game(game)
         if winner:
+            print("\nWINNER")
             for d in game:
                 print(f" {d}")
-            input("WINNER")
+            calc_score(game, draw)
             exit(0)
 
+def calc_score(game, draw):
+    tot = 0
+    for data in game:
+        for k,v in data.items():
+            if not v:
+                tot += int(k)
+            
+    print(f"Final score {tot * int(draw)}")
+
+
 def check_game(game):
-    tmp = []
-    #print(game[:5])
-    for i in range(len(game[:5])):
-        print(i)
-        for j in game[5]["draws"]:
-            try:
-                data = game[i][j]
-                #print("check_game ", game[i])
-                #print("check for  ", j)
-                #print("found at pos ", data)
-                calc = (i * 5) + data
-                tmp.append(calc)
-                #print("at pos: ", calc)
-                
-                if len(tmp) >= 5:
-                    #print("checking for win.. ", tmp)
-                    #input("")
-                    if check_row_win(tmp): # or check_col_win(tmp):
-                        #print("win\n", game)
-                        return True
-                    
-            except KeyError:
-                pass
-
-def check_row_win(tmp):
-    tmp.sort()
-    #print("check rows")
-    print("check rows tmp: ", tmp)
-
-    n = len(tmp)
-    k = 5
-
-    for i in range(n-k+1):
-        current_sum = 0
-        prev = -1
-        for j in range(k):
-            print("prev ", prev)
-            print("new - 1: ", tmp[i + j]-1)
-            if prev > 0 and tmp[i + j]-1 != prev:
-                prev = tmp[i + j]
-                current_sum = prev
-                continue
-            current_sum = current_sum + int(tmp[i + j])
-            prev = tmp[i + j]
-
-        #print(f"curr: {current_sum}")
-        
-        if current_sum >= 10 and (current_sum) % 5 == 0:
-            print("ROW WIN", current_sum)
-            return True
-    
+    col_list = ["", "", "", "", ""]
+    for data in game:
+        row = ""
+        for k, i in zip(data, range(5)):
+            row += data[k]
+            col_list[i] += data[k]
+            if len(row) == 5 or len(col_list[i]) == 5:
+                return True
     return False
-        
-def check_col_win(tmp):
-    tmp.sort()
-    #print("check cols")
-    #print("check cols tmp: ", tmp)
 
-    n = len(tmp)
-    k = 5
-
-    
-    for i in range(n-k+1):
-        current_sum = 0
-        prev = -1
-        for j in range(k):    
-            if prev > 0 and tmp[i + j] == prev + 5:
-                current_sum = 0
-                break
-            current_sum = current_sum + int(tmp[i + j])
-            prev = tmp[i + j]
-
-        print(f"curr: {current_sum}")
-        
-        if current_sum >= 50 and (current_sum - 50) % 5 == 0:
-            print("COL WIN")
-            return True
-    
-    return False
+def check_col(game, idx):
+    return game[idx] + game[idx] + game[idx] + game[idx] + game[idx]
 
 if __name__ == "__main__":
     draw_order, guess_list = readfile()
     print("start guess")
     print_guess(guess_list)
     draw_nr(draw_order, guess_list)
-    #print(draw_order)
-    #print(guess_list)
 
 
 
