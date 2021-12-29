d8baceea58bb8c48e3841f5df88c2b52b95b0a5c
looten <olsson.joel@live.com>
2021-12-29 14:58:31 +0100

    2021: Day 8 part 2 working

--- 2021/day8/day8.py
+++ 2021/day8/day8.py
@@ -1,16 +1,16 @@
 
-filename = "/home/looten/workspace/advent_of_code/2021/day8/test_data2.txt"
-#filename = "/home/looten/workspace/advent_of_code/2021/day8/input.txt"
-SEVEN_DIGIT_MAPPING = {"zero": "abcefg",
-                       "one": "cf",
-                       "two": "acdeg",
-                       "three": "acdfg",
-                       "four": "bcdf",
-                       "five": "abdfg",
-                       "six": "abdefg",
-                       "seven": "acf",
-                       "eight": "abcdefg",
-                       "nine": "abcdfg"}
+#filename = "/home/looten/workspace/advent_of_code/2021/day8/test_data2.txt"
+filename = "/home/looten/workspace/advent_of_code/2021/day8/input.txt"
+SEVEN_DIGIT_MAPPING = {"zero": "0",
+                       "one": "1",
+                       "two": "2",
+                       "three": "3",
+                       "four": "4",
+                       "five": "5",
+                       "six": "6",
+                       "seven": "7",
+                       "eight": "8",
+                       "nine": "9"}
 
 
 def read_input():
@@ -31,47 +31,46 @@ def read_input():
 
 def decode(input_data: list, output_data: list):
     mapping = {"complete": False,
-               "0": {"zero": "", "cnt": 0},
-               "1": {"one": "", "cnt": 0},
-               "2": {"two": "", "cnt": 0},
-               "3": {"three": "", "cnt": 0},
-               "4": {"four": "", "cnt": 0},
-               "5": {"five": "", "cnt": 0},
-               "6": {"six": "", "cnt": 0},
-               "7": {"seven": "", "cnt": 0},
-               "8": {"eight": "", "cnt": 0},
-               "9": {"nine": "", "cnt": 0}, }
-    #decoded_data = [mapping, mapping, mapping, mapping]
+               "0": {"zero": ""},
+               "1": {"one": ""},
+               "2": {"two": ""},
+               "3": {"three": ""},
+               "4": {"four": ""},
+               "5": {"five": ""},
+               "6": {"six": ""},
+               "7": {"seven": ""},
+               "8": {"eight": ""},
+               "9": {"nine": ""}, }
     undecoded_data = []
-    print(len(undecoded_data))
-    row = 0
+    tot = 0
     for input_d, output_d in zip(input_data, output_data):
-        row += 1
-        print("row", row)
         for o in output_d:
             o = "".join(sorted(o))
             for i in input_d:
                 i = "".join(sorted(i))
-                #print("in", i)
                 added = easy_decode(mapping, i)
                 if not added and o == i:
                     undecoded_data.append(i)
-    
+
         for undec in undecoded_data:
             map_the_rest(mapping, undec)
 
+        value = ""
         for o in output_d:
             o = "".join(sorted(o))
-            print(final_dec(mapping, o))
+            tmp = final_dec(mapping, o).keys()
+            for key in tmp:
+                tmp = str(key)
+            value += SEVEN_DIGIT_MAPPING[tmp]
+        tot += int(value)
+        #print(value + "\n")
 
-        #for undec in undecoded_data:
-            #count_other(mapping, undec)
         undecoded_data.clear()
         reset(mapping)
-    return mapping
+    return mapping, tot
 
 
-def reset(data : dict):
+def reset(data: dict):
     data["0"]["zero"] = ""
     data["1"]["one"] = ""
     data["2"]["two"] = ""
@@ -111,60 +110,34 @@ def final_dec(mapping: dict, i: str):
         print(f"input {i}")
         exit(0)
 
+
 def print_mappning(mapping):
     for k in mapping:
         print(f" {k} : {mapping[k]}")
 
+
 def easy_decode(mapping: dict, i: str):
     if len(i) == 2:
         mapping["1"]["one"] = i
-        mapping["1"]["cnt"] += 1
         return True
     elif len(i) == 4:
         mapping["4"]["four"] = i
-        mapping["4"]["cnt"] += 1
         return True
     elif len(i) == 3:
         mapping["7"]["seven"] = i
-        mapping["7"]["cnt"] += 1
         return True
     elif len(i) == 7:
         mapping["8"]["eight"] = i
-        mapping["8"]["cnt"] += 1
         return True
     return False
 
 
-def count_other(mapping: dict, i: str):
-    if mapping["0"]["zero"] == i:
-        mapping["0"]["cnt"] += 1
-
-    elif mapping["2"]["two"] == i:
-        mapping["2"]["cnt"] += 1
-
-    elif mapping["3"]["three"] == i:
-        mapping["3"]["cnt"] += 1
-
-    elif mapping["5"]["five"] == i:
-        mapping["5"]["cnt"] += 1
-
-    elif mapping["6"]["six"] == i:
-        mapping["6"]["cnt"] += 1
-
-    elif mapping["9"]["nine"] == i:
-        mapping["9"]["cnt"] += 1
-
-    else:
-        print("what?")
-        exit(0)
-
-
 def map_the_rest(data: dict, i: str):
     # 9
-    #if not data["9"]["nine"]:
-    tmp = data["4"]["four"] + data["7"]["seven"]
-    tmp = "".join(sorted(set(tmp)))
-    data["9"]["nine"] = tmp
+    nine = get_rest(i, data["8"]["eight"])
+    nine_2 = get_rest(data["4"]["four"], i)
+    if len(nine) == 1 and len(nine_2) == 2:
+        data["9"]["nine"] = i
 
     # 3
     three = get_rest(data["4"]["four"], i)
@@ -172,81 +145,43 @@ def map_the_rest(data: dict, i: str):
     if len(three) == 2 and len(three_2) == 2:
         data["3"]["three"] = i
 
-    six = get_rest(data["8"]["eight"], i)
     # 6
-    if len(six)== 1:
+    six = get_rest(i, data["8"]["eight"])
+    six_2 = get_rest(data["7"]["seven"], i)
+    six_3 = get_rest(data["1"]["one"], i)
+    if len(six) == 1 and len(six_2) == 4 and len(six_3) == 5:
         data["6"]["six"] = i
 
-    zero = get_rest(data["9"]["nine"], i)
     # 0
-    if len(zero) == 2:
+    zero = get_rest(data["1"]["one"], i)
+    zero_2 = get_rest(data["4"]["four"], i)
+    zero_3 = get_rest(i, data["8"]["eight"])
+    if len(zero) == 4 and len(zero_2) == 3 and len(zero_3) == 1:
         data["0"]["zero"] = i
-    
-    five = get_rest(data["9"]["nine"], i)
-    five_2 = get_rest(data["4"]["four"], i)
+
     # 5
-    if len(five) == 1 and len(five_2) == 2:
+    five = get_rest(data["1"]["one"], i)
+    five_2 = get_rest(data["4"]["four"], i)
+    five_3 = get_rest(i, data["8"]["eight"])
+    if len(five) == 4 and len(five_2) == 2 and len(five_3) == 2:
         data["5"]["five"] = i
 
-   
-    two = get_rest(data["9"]["nine"], i)
+    # 2
+    two = get_rest(data["7"]["seven"], i)
     two_2 = get_rest(data["4"]["four"], i)
-     # 2
-    if len(two) == 1 and len(two_2) == 3:
+    two_3 = get_rest(data["1"]["one"], i)
+    two_4 = get_rest(i, data["8"]["eight"])
+    if len(two) == 3 and len(two_2) == 3 and len(two_3) == 4 and len(two_4) == 2:
         data["2"]["two"] = i
-
-    if False:
-        if data["6"]["six"]:
-            data["0"]["zero"] = i
-            # 3
-            print("found a 3")
-            a = get_rest(data["4"]["four"], data["5"]["five"])
-            b = get_rest(data["0"]["zero"], data["8"]["eight"])
-            data["3"]["three"] = ''.join(sorted(set(a + b + data["1"]["one"])))
-            
-            # 5
-            a = get_rest(data["6"]["six"], data["3"]["three"])
-            print(data["3"]["three"])
-            print(data["6"]["six"])
-            b = get_rest(a, data["4"]["four"])
-            data["5"]["five"] = ''.join(sorted(set(a + b)))
-            print(a)
-            print(b)
-            print("five", data["5"]["five"])
-            input()
-
-
-            print("found a 2")
-            # 2
-            a = get_rest(data["9"]["nine"], data["6"]["six"])
-            b = get_rest(data["0"]["zero"], data["8"]["eight"])
-            print("found a 2 a)")
-            c = get_rest(data["1"]["one"], data["7"]["seven"])
-            d = get_rest(data["3"]["three"], data["5"]["five"])
-            data["2"]["two"] = ''.join(sorted(set(a + b + c + d)))
 
 
 def get_rest(sub_str: str, res: str):
-    #if len(sub_str) > len(res):
-     #   print("Error: sub str longer that res")
-      #  exit(0)
     for c in sub_str:
         res = res.replace(c, "")
     return res
 
 
-def count(data: dict):
-    tot = 0
-    #for data in decoded_data:
-    tot += data["0"]["cnt"] + data["1"]["cnt"] + \
-        data["2"]["cnt"] + data["3"]["cnt"] + \
-        data["4"]["cnt"] + data["5"]["cnt"] + \
-        data["6"]["cnt"] + data["7"]["cnt"] + \
-        data["8"]["cnt"] + data["9"]["cnt"]
-    print(f"Instances: {tot}")
-
-
 if __name__ == "__main__":
     input_data, output_data = read_input()
-    decoded_data = decode(input_data, output_data)
-    count(decoded_data)
+    decoded_data, tot = decode(input_data, output_data)
+    print(tot)
