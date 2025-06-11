e8a9f7f29a9d6569002191fb148b5354e22d649a
Joel Olsson <olsson.joel@live.com>
2025-06-11 16:55:15 +0000

    day11: looked up solu for part2

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -2,6 +2,7 @@ import os
 import copy
 import time
 import pandas as pd
+from functools import cache
 
 def read_input(filename):
     file_path = os.path.abspath(os.path.dirname(__file__))
@@ -13,58 +14,101 @@ def read_input(filename):
                 input_data.append(int(u))
     return input_data
 
+
+def check_mult(d, temp_res, num_zeroes, num_ones, num_mults):
+    #print("split")
+    q, r = divmod(len(str(d)), 2)
+    d_st = str(d)
+    first, sec = d_st[:q + r], d_st[q + r:]
+    #print("first ", int(first))
+    #print("sec ", int(sec))
+    if int(first) == 0:
+        num_zeroes += 1
+    elif int(first) == 1:
+        num_ones += 1
+    else:
+        temp_res.append(int(first))
+
+    if int(sec) == 0:
+        num_zeroes += 1
+    elif int(sec) == 1:
+        num_ones += 1
+    else:
+        temp_res.append(int(sec))
+    
+    return temp_res, num_zeroes, num_ones, num_mults
+
+@cache
+def count(stone, steps):
+    # end of recurs
+    print("call for stone ", stone)
+    if steps == 0:
+        print(" bottom! for stone ", stone)
+        return 1
+
+    if stone == 0:
+        return count(1, steps-1)
+
+    string = str(stone)
+    length = len(string)
+    if length % 2 == 0:
+        return count(int(string[:length // 2]), steps-1) + count(int(string[length // 2:]), steps-1)
+
+    return count(stone*2024, steps-1)
+    
+
 def stone_rule_exec(input_data, blinks):
     print("Initial arrangement:")
     print(input_data)
-    blink_results = input_data
-    data = {'temp': blink_results}
-    #data2 = {'temp': []}
 
-    df = pd.DataFrame(data)
-    df_temp = pd.DataFrame(data=[],columns=['temp'])
-    
-    df['temp'] = pd.to_numeric(df['temp'], downcast='integer')
-    df_temp['temp'] = pd.to_numeric(df_temp['temp'], downcast='integer')
+    blink_results = input_data
+    temp_res = []
+
+    num_zeroes = blink_results.count(0)
+    num_ones = blink_results.count(1)
+    num_mults = blink_results.count(2024)
+
+    blink_results  = [i for i in blink_results if i != 0 or i != 1 or i != 2024]
 
     start = time.time()
     for blink in range(blinks):
-        df_temp = pd.DataFrame(data=[],columns=['temp'])
-        for _, row in df.iterrows():
-            #print("new loop ")
-            #print(df_temp)
-            #print("df ")
-            #print(df)
-            d = row["temp"]
-            #print(" curr data ", d)
+        temp_res = []
+        for _ in range(num_mults):
+            temp_res, num_zeroes, num_ones, num_mults = check_mult(2024, temp_res, num_zeroes, num_ones, num_mults)
+        for d in blink_results:
+            d = int(d)
             if d == 0:
                 #print("zero")
-                df_temp = df_temp._append({"temp": 1}, ignore_index=True)
+                num_zeroes += 1
+            elif d == 1:
+                #print("one")
+                num_ones += 1
             elif len(str(d)) % 2 == 0:
-                #print("split")
-                q, r = divmod(len(str(d)), 2)
-                d_st = str(d)
-                first, sec = d_st[:q + r], d_st[q + r:]
-
-                #print("first ", int(first))
-                #print("sec ", int(sec))
-                df_temp = df_temp._append({"temp": int(first)}, ignore_index=True)
-                df_temp = df_temp._append({"temp": int(sec)}, ignore_index=True)
+                #print("mult")
+                temp_res, num_zeroes, num_ones, num_mults = check_mult(d, temp_res, num_zeroes, num_ones, num_mults)
             else:
                 #print("mult")
-                df_temp = df_temp._append({"temp": int(d*2024)}, ignore_index=True)
-        
-        #print("COMPLETE")
-        #print(df_temp)
-        df = df_temp.copy(deep=True)
-        df.dropna(inplace = True)
-        #print(df)
+                d *= 2024
+                temp_res.append(int(d))
+        #print("zeroes ", num_zeroes)
+
         print(f"After {blink+1} blink/s: {time.time()-start} ")
-        #print(" ".join(map(str, blink_results)))
-        #print("df ")
-        #print(df)
-    print("df ")
-    print(df)
-    print("df len ", len(df))
+        blink_results = copy.deepcopy(temp_res)
+        #print(blink_results)
+        num_ones = num_zeroes
+        #print("0 ", num_zeroes)
+        #print("1 ", num_ones)
+        #print("mult ", num_mults)
+        num_mults = num_ones
+        num_zeroes = 0
+
+        #input()
+
+        
+        
+    
+    print(blink_results)
+    print(len(blink_results) + num_zeroes + num_ones + num_mults)
 
 if __name__ == "__main__":
     filename = "sample_input1.txt"
@@ -72,9 +116,9 @@ if __name__ == "__main__":
 
     filename = "sample_input2.txt"
     blinks = 6
-
+#
     filename = "input.txt"
     blinks = 75
 
     input_data = read_input(filename)
-    stone_rule_exec(input_data, blinks)
+    print(sum(count(stone, blinks) for stone in input_data))
