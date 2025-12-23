940c1306263bd3da2c67c82d3c582c6b04e23b84
Joel Olsson <olsson.joel@live.com>
2025-12-23 23:43:49 +0100

    2025: day3 part 2 almost works, a bit of rework ..

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -19,23 +19,35 @@ def decode(data):
     answer = 0
     for d in data:
         print(d)
-        first = -1
-        last = -1
+        tmp = []
         for idx, i in enumerate(d):
-            #print(f"idx: {idx}, i: {i}, first: {first}, last: {last}")
-            if i > first and idx + 1 < len(d):
-                first = i
-                last = -1
-            elif i > last:
-                last = i
-        print(f"first: {first}, last: {last}")
-        answer += int(str(first) + str(last))
+            print(f"start idx: {idx}, i: {i}, tmp {tmp} len tmp {len(tmp)}  len(d) - (idx + 1): {len(d) - (idx + 1)}")
+            if len(tmp) < 12:
+                tmp.append(i)
+                print(f"start tmp {tmp}")
+                if len(tmp) > 1:
+                    print(f"{len(d) - (idx + 1 + len(tmp)) > 6 } {len(tmp) >=2} { tmp[-1] > tmp[-2]}")
+              #  if idx < 7 and len(d) - (idx + 1) >= 6 and len(tmp) >=2 and tmp[-1] > tmp[-2]:
+                if len(d) - (idx + 1 + len(tmp)) > 6 and len(tmp) >=2 and tmp[-1] > tmp[-2]:
+                    new_first = tmp[-1]
+                    tmp.pop()
+                    tmp.pop()
+                    tmp.append(new_first)
+                    print(f"swapped idx: {idx}, i: {i}, tmp {tmp} len tmp {len(tmp)}")
 
-            
+            elif i > tmp[-1]:
+                tmp.pop()
+                tmp.append(i)
+        t_str = ""
+        for t in tmp:
+            t_str += str(t)
+        print("len tmp:", len(t_str))
+        print(t_str)
+        answer += int(str(t_str))
     print(f"Answer: {answer}")
 
 if __name__ == "__main__":
     filename = "sample_input.txt"
-    filename = "input.txt"
+    #filename = "input.txt"
     data = read_input(filename)
     decode(data)
