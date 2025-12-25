96f917814f41ac64fe1fce7a3d0bd80fd2e75ae6
Joel Olsson <olsson.joel@live.com>
2025-12-25 01:38:56 +0100

    2025: day4 part2 complete

--- 2025/day4/day4.py
+++ 2025/day4/day4.py
@@ -38,32 +38,45 @@ def decode(data):
         (-1, 1),    # NW
         ]
     answer = 0
-    res_map = copy.deepcopy(data)
-    for row_idx, row in enumerate(data):
-        print(row)
-        for col_idx, col in enumerate(row):
-            if col == "@":
-                print(f"start search ({row_idx,col_idx}) at:", )
-                #start_idx = (row_idx, col_idx)
-                num_sur_paper_rolls = 0
-                for direction in directions:
-                    x, y = direction
-                    in_bounds = row_idx + y >= 0 and row_idx + y < len(data) and col_idx + x >= 0 and col_idx + x < len(row)
-                    if in_bounds and data[row_idx + y][col_idx + x] == "@":
-                        num_sur_paper_rolls += 1
-                        print(f"found in direction:({direction}) cords = ({row_idx + y, col_idx + x})")
-               #print(f"Num surrounding paper rolls: {num_sur_paper_rolls}")
-                if num_sur_paper_rolls < 4:
-                    print(f"OK, only {num_sur_paper_rolls} surrounding")
-                    res_map[row_idx][col_idx] = "x"
-                    answer += 1
-                else:
-                    print(f"Too many surrounding ({num_sur_paper_rolls})")
-                #input()
+    sub_ans = 0
+    found = True
+    while found:
+        res_map = copy.deepcopy(data)
+        sub_ans = 0
+
+        for row_idx, row in enumerate(data):
+           # print(row)
+            for col_idx, col in enumerate(row):
+                if col == "@":
+                    #print(f"start search ({row_idx,col_idx}) at:", )
+                    #start_idx = (row_idx, col_idx)
+                    num_sur_paper_rolls = 0
+                    for direction in directions:
+                        x, y = direction
+                        in_bounds = row_idx + y >= 0 and row_idx + y < len(data) and col_idx + x >= 0 and col_idx + x < len(row)
+                        if in_bounds and data[row_idx + y][col_idx + x] == "@":
+                            num_sur_paper_rolls += 1
+                          #  print(f"found in direction:({direction}) cords = ({row_idx + y, col_idx + x})")
+                #print(f"Num surrounding paper rolls: {num_sur_paper_rolls}")
+                    if num_sur_paper_rolls < 4:
+                       # print(f"OK, only {num_sur_paper_rolls} surrounding")
+                        res_map[row_idx][col_idx] = "x"
+                        answer += 1
+                        sub_ans += 1
+                    #else:
+                       # print(f"Too many surrounding ({num_sur_paper_rolls})")
+                    #input()
+        if sub_ans == 0:
+            found = False
+        else:
+            data = copy.deepcopy(res_map)
+           # print(f"Remove {sub_ans} paper rolls:")
+            #for row in res_map:
+            #    print("".join(row))
+            #input()
 
     print(f"Answer: {answer}")
-    #for row in res_map:
-     #   print("".join(row))
+
 
 if __name__ == "__main__":
     filename = "sample_input.txt"
